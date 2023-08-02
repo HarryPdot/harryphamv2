f057b1e8b070893591c3a1acdf20afa6d530ed86
Harry <harrypham_@hotmail.com>
2023-08-02 18:00:06 +1000

    yeah..

--- src/Components/Character/Character.tsx
+++ src/Components/Character/Character.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect, useState } from 'react';
 
-export const Character = ({ data }) => {
+export const Character = ({ data }: any) => {
   const [character, setCharacter] = useState<string>('');
 
   useEffect(() => {
