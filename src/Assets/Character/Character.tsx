b49c615e42c5c77d3cdc623b83919e2106cfa9a3
Harry <harrypham_@hotmail.com>
2023-08-02 17:30:18 +1000

    add any type

--- src/Assets/Character/Character.tsx
+++ src/Assets/Character/Character.tsx
@@ -5,7 +5,7 @@ import useSWR, { useSWRConfig } from 'swr';
 export const Character = () => {
   const [char, setChar] = useState<string>('');
   const url = `https://maplestory.io/api/character/%7B%22itemId%22%3A2000%2C%22version%22%3A%22235%22%7D%2C%7B%22itemId%22%3A12000%2C%22version%22%3A%22235%22%7D%2C%7B%22itemId%22%3A1053252%2C%22version%22%3A%22235%22%7D%2C%7B%22itemId%22%3A1005061%2C%22version%22%3A%22235%22%7D%2C%7B%22itemId%22%3A26703%2C%22animationName%22%3A%22default%22%2C%22version%22%3A%22235%22%7D/swingO1/animated?showears=false&showLefEars=false&showHighLefEars=undefined&resize=1&name=&flipX=false&bgColor=0,0,0,0`;
-  const fetcher = (...args) =>
+  const fetcher = (...args: any) =>
     fetch(...args).then((res) => {
       return res.blob();
     });
